cfc8865a7e80013c4271ecef3ed8f3ee504aa5cd
Alexandr Yeremieiev <sanya@hackermail.com>
2021-12-05 00:34:52 +0100

    6 ДЗ без проверки на нажатие отмена

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert('hello')
+let question;
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+};
+function gameBot(num) {
+
+
+    function two() {
+        question = parseFloat(prompt('Угадай число от 1 до 100'));
+        if (question === num) {
+            alert('Поздравляю, Вы угадали!!! ' + 'Число: ' + question)
+        }
+        else if (!isNumber(question)) {
+            alert('Введите число');
+            two()
+        }
+        else if (question > num) {
+            alert('Загаданное число меньше');
+            two()
+        }
+        else if (question < num) {
+            alert('Загаданное число больше');
+            two()
+        }
+        else if (question === num) {
+            alert('Поздравляю, Вы угадали!!! ' + 'Число: ' + question)
+        }
+    }
+    two()
+}
+
+gameBot(Math.floor(Math.random() * 100));
