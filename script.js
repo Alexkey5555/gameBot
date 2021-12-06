53a521d769904ddfcd6d745af3a580512d85b87e
Alexandr Yeremieiev <sanya@hackermail.com>
2021-12-06 21:43:27 +0100

    выполнено сложное задание урока 6

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 };
 function gameBot(num) {
+    let count = 10;
+    let attempt;
     function two() {
         let answer = prompt('Угадай число от 1 до 100');
         if (answer === null) {
@@ -11,18 +13,37 @@ function gameBot(num) {
         }
         question = parseFloat(answer);
         if (question === num) {
-            alert('Поздравляю, Вы угадали!!! ' + 'Число: ' + question)
+            attempt = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')
+            if (attempt === true) {
+                gameBot(Math.floor(Math.random() * 100));
+            }
+            else {
+                alert('Игра окончена');
+                return;
+            }
+        }
+        else if (count == 1) {
+            attempt = confirm('Попытки закончились, хотите сыграть еще?')
+            if (attempt === true) {
+                gameBot(Math.floor(Math.random() * 100));
+            }
+            else {
+                alert('Игра окончена');
+                return;
+            }
         }
         else if (!isNumber(question)) {
             alert('Введите число');
             two()
         }
         else if (question > num) {
-            alert('Загаданное число меньше');
+            count--;
+            alert('Загаданное число меньше, осталось попыток ' + count);
             two()
         }
         else if (question < num) {
-            alert('Загаданное число больше');
+            count--;
+            alert('Загаданное число больше, осталось попыток ' + count);
             two()
         }
     }
